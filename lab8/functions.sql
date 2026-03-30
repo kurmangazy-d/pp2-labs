712cc4f7f0fcf0656692332766514b89ea4129e2
dann <dqurmangazy@dana-2.local>
2026-03-30 13:41:34 +0500

    added

--- lab8/functions.sql
+++ lab8/functions.sql
@@ -1,22 +1,10 @@
-CREATE OR REPLACE FUNCTION get_contacts_by_pattern(p TEXT)
-RETURNS TABLE(name TEXT, surname TEXT, phone TEXT) AS $$
-BEGIN
-    RETURN QUERY
-    SELECT c.name, c.surname, c.phone
-    FROM contacts c
-    WHERE c.name ILIKE '%' || p || '%'
-       OR c.surname ILIKE '%' || p || '%'
-       OR c.phone ILIKE '%' || p || '%';
-END;
-$$ LANGUAGE plpgsql;
-
-
 CREATE OR REPLACE FUNCTION get_contacts_paginated(p_limit INT, p_offset INT)
 RETURNS TABLE(name TEXT, surname TEXT, phone TEXT) AS $$
 BEGIN
     RETURN QUERY
     SELECT c.name, c.surname, c.phone
     FROM contacts c
+    ORDER BY c.name
     LIMIT p_limit OFFSET p_offset;
 END;
 $$ LANGUAGE plpgsql;
